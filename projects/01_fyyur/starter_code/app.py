733954e94b1e7498edd2897b691ea66c7a799781
lb <69806056+lb@users.noreply.github.com>
2021-06-29 18:13:48 -0400

    remove unnecessary things

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -310,11 +310,6 @@ def create_venue_submission():
       db.session.close()
   else:
     error=True
-    error_message=[]
-    for field, err in form.errors.items():
-      error_message.append(f"{field} {'|'.join(err)}")
-    if len(error_message)>1:
-      flash('Errors ' + str(error_message))
 
   # on successful db insert, flash success
 
@@ -690,17 +685,10 @@ def create_artist_submission():
     except:
       error=True
       db.session.rollback()
-      print(sys.exc_info())
-
     finally:
       db.session.close()
   else:
     error=True
-    error_message=[]
-    for field, err in form.errors.items():
-      error_message.append(f"{field} {'|'.join(err)}")
-    if len(error_message)>1:
-      flash('Errors ' + str(error_message))
 
 
   # TODO: modify data to be the data object returned from db insertion
